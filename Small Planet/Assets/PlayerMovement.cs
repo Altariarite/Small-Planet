f68a10caa6ba973026004904f2ef6f969fbee2b4
Altariarite <jimmyoneblue@gmail.com>
2017-08-24 14:32:10 +0800

    ver.1
    bad control

---
+++ Small Planet/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody playerRigidbody;
+    Vector3 movement;
+
+    [SerializeField]
+    float force;
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        
+
+        Move(h, v);
+
+    }
+    void Move(float h, float v)
+    {
+        playerRigidbody.AddForce(h*force*Time.deltaTime, 0, v*force*Time.deltaTime,ForceMode.VelocityChange); 
+    }
+
+}
