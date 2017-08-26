99967d52218e5ea5b7580b2420fe89b40f25e960
Altariarite <jimmyoneblue@gmail.com>
2017-08-26 22:04:26 +0800

    ver4.1
    turret target follow complete
    fixed assets

--- Small Planet/Assets/Scripts/Turret.cs
+++ Small Planet/Assets/Scripts/Turret.cs
@@ -3,9 +3,19 @@
 public class Turret : MonoBehaviour {
 
     private Transform target;
-    public float range = 15f;
 
     public string enemyTag = "Enemy";
+    
+    [Header("Unity Setup")]
+    public Transform partToRotate;
+    public float turnspeed = 1f;
+
+    [Header("Attributes")]
+    public float range = 15f;
+    public float fireRate = 1f;
+    public float fireCountdown = 0f;
+
+
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -41,10 +51,26 @@ public class Turret : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (target == null)
-            return;	
+            return;
+
+        //Target lock on
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime*turnspeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(rotation);
 
 
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+          
+        }
 	}
+
+    void Shoot ()
+    {
+
+    }
 
     private void OnDrawGizmosSelected()
     {
